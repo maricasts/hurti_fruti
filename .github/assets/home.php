43315643d25315b840bf9f0b6bf2d8dd6d367dff
maricasts <mariaimaculada7773@gmail.com>
2024-11-24 11:55:05 -0300

    mudança no titulo

--- .github/assets/home.php
+++ .github/assets/home.php
@@ -17,7 +17,7 @@ if (!isset($_SESSION['email'])) {
 <head>
     <meta charset="UTF-8"/>
     <meta content="width=device-width, initial-scale=1.0"/>
-    <title>Hurti Fruti - Frutas para Provar</title>
+    <title>Selecionando frutas</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
